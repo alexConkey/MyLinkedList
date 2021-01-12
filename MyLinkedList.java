9eb9cc96dc25247026f07cfdd5ace208420f7b05
Alex Conkey <aconkey20@stuy.edu>
2021-01-12 07:57:15 -0500

    created size and first add methods, skipped constructor for now

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,10 +1,20 @@
 public class MyLinkedList{
  private int size;
  private Node start,end;
- public MyLinkedList(){/*create a constructor*/}
- public int size();
- public boolean add(String value);
- public void add(int index, String value);
+ public MyLinkedList(){
+
+ }
+ public int size(){
+   return size;
+ }
+ public boolean add(String value){
+   Node newNode = new Node(value);
+   end.setNext(newNode);
+   end = end.getNext();
+ }
+ public void add(int index, String value){
+   
+ }
  public String get(int index);
  public String set(int index, String value);
  public String toString();
