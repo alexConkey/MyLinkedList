7982eb9593b04f6507580e4b6199f5e658c04ad6
Alex Conkey <aconkey20@stuy.edu>
2021-01-12 07:46:55 -0500

    Created constructor and set up other functions

--- Node.java
+++ Node.java
@@ -1,6 +1,16 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
- private String data;
- private Node next,prev;
+  private String data;
+  private Node next,prev;
+  public Node(String value){
+    data = value;
+  }
+
+  private Node next,prev;
+  public String getData(){}
+  public String getNext(){}
+  public String getPrev(){}
+  public String setData(){}
+  public String setNext(){}
+  public String setPrev(){}
  //write get/set methods for all three instance variables.
 }
